1f0cf2e0900b1110bc693327bae3e4e0cafaea01
Waqasali.CE <waqas.ali@codeencoders.com>
2025-11-28 13:05:11 +0500

    feat: intro section completed, gsap added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Description from "@/components/description";
 import Intro from "@/components/intro";
@@ -6,21 +6,18 @@ import Projects from "@/components/projects";
 import { useEffect } from "react";
 
 export default function Home() {
-
   useEffect(() => {
-   (
-    async () => {
+    (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
-    }
-   )()
-  }, [])
-  
+    })();
+  }, []);
+
   return (
-   <main className="flex flex-col gap-5">
+    <main className="flex flex-col gap-5">
       <Intro />
-      <Description />
-      <Projects />
-   </main>
+      {/* <Description />
+      <Projects /> */}
+    </main>
   );
 }
